2a7025e55770153fdd72419e3565017cc50718d0
Michael D. Hoyle <hoylemd@gmail.com>
2012-07-15 15:59:24 -0400

    Added messaging structure for keyboard inputs

---
+++ client/scripts/input-functions.js
@@ -0,0 +1,16 @@
+keyActions[37] = function()
+{
+	alert("left");
+}
+keyActions[38] = function()
+{
+	alert("up");
+}
+keyActions[39] = function()
+{
+	alert("right");
+}
+keyActions[40] = function()
+{
+	alert("down");
+}
