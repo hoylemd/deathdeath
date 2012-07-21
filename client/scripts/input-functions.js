729de4ea4a2404d21e9ed926ab8a85e92a6fae94
hoylemd <hoylemd@gmail.com>
2012-07-21 13:09:42 -0400

    move entities into own global object

--- client/scripts/input-functions.js
+++ client/scripts/input-functions.js
@@ -10,34 +10,33 @@ keyActions[17] = function()
 keyActions[37] = function()
 {
 	// Move player and redraw sprites
-	dd.player.move(-1, 0);
+	en.player.move(-1, 0);
 	dd.processTick();
 }
 // Up Arrow Key
 keyActions[38] = function()
 {
 	// Move player and redraw sprites
-	dd.player.move(0, -1);
+	en.player.move(0, -1);
 	dd.processTick();
 }
 // Right Arrow Key
 keyActions[39] = function()
 {
 	// Move player and redraw sprites
-	dd.player.move(1, 0);
+	en.player.move(1, 0);
 	dd.processTick();
 }
 // Down Arrow Key
 keyActions[40] = function()
 {
 	// Move player and redraw sprites
-	dd.player.move(0, 1);
+	en.player.move(0, 1);
 	dd.processTick();
 }
 // 1 key
 keyActions[49] = function()
 {
-	alert("1");
-	sp.fireball(dd.enemy);
+	sp.fireball(en.enemy);
 	dd.processTick();
 }
